89709ad49efc8dd1eddc0e2bb3e3e3259486eff3
ZEYNEP NİDA SARICAN <56023452+zynpnd@users.noreply.github.com>
2026-01-30 16:48:37 +0300

    fix next config for render static export

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   output: "export",
-   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cryptologos.cc",
-      },
-    ],
+  output: "export",
+  images: {
+    unoptimized: true,
   },
-
 };
 
 export default nextConfig;
-
